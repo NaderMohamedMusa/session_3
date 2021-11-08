ab29000a5e5b316e6d66ec7d8f15fae7e84a1f55
Nader mohamed <nm01123578062@gmail.com>
2021-11-08 20:03:11 +0200

    add design 9

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -100,10 +100,10 @@ class HomeScreen extends StatelessWidget {
           ),
         ],
       ),
-
       body:
 
-      ///CircleAvatar & TextFormField && form with column & padding & InkWell && Container (design 8)
+
+      ///CircleAvatar & TextFormField && form with column & padding & InkWell && Container (design 9)
       // Form(
       //   key: _keyForm,
       //   child: Column(
@@ -118,10 +118,10 @@ class HomeScreen extends StatelessWidget {
       //           padding: const EdgeInsets.all(5.0),
       //           child: Container(
       //             decoration: BoxDecoration(
-      //               shape: BoxShape.circle,
-      //               image: DecorationImage(
+      //                 shape: BoxShape.circle,
+      //                 image: DecorationImage(
       //                   image: AssetImage("assets/images/nad.png"),
-      //               )
+      //                 )
       //             ),
       //           ),
       //         ),
@@ -234,11 +234,179 @@ class HomeScreen extends StatelessWidget {
       //             ),
       //           ),
       //         ),
-      //       )
+      //       ),
+      //
+      //       Row(
+      //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      //         children: [
+      //           Container(
+      //             color: Colors.black,
+      //             height: 1,
+      //             width: MediaQuery.of(context).size.width*0.43,
+      //           ),
+      //           CircleAvatar(
+      //             backgroundColor: Colors.black12,
+      //             child: Text("OR",
+      //               style: TextStyle(color: Colors.black,fontSize: 16),),
+      //           ),
+      //           Container(
+      //             color: Colors.black,
+      //             height: 1,
+      //             width: MediaQuery.of(context).size.width*0.43,
+      //           ),
+      //         ],
+      //       ),
+      //
+      //
+      //       Row(
+      //         mainAxisAlignment: MainAxisAlignment.center,
+      //         children: [
+      //           Text("Don't have an Account?  ",
+      //             style: TextStyle(color: Colors.black,fontSize: 17),),
+      //           Text("Sign Up",
+      //             style: TextStyle(color: Colors.teal,fontSize: 16),),
+      //         ],
+      //       ),
       //     ],
       //   ),
       // ),
 
+
+      ///CircleAvatar & TextFormField && form with column & padding & InkWell && Container (design 8)
+      Form(
+        key: _keyForm,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+
+            CircleAvatar(
+              radius: 80,
+              backgroundColor: Colors.black12,
+              child: Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                        image: AssetImage("assets/images/nad.png"),
+                    )
+                  ),
+                ),
+              ),
+            ),
+
+            SizedBox(height: 20),
+
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                controller: email,
+                keyboardType: TextInputType.emailAddress,
+                validator: (value){
+                  if(value!.isEmpty){
+                    return "Enter Your mail";
+                  }
+                  return null;
+                },
+                onChanged: (value){
+                  print(value);
+                },
+                onSaved: (value){
+                  mail = value!;
+                },
+                decoration: InputDecoration(
+                  hintText: "Enter your mail",
+                  labelText: "Email",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: BorderSide(width: 5,
+                        color: Colors.green),
+                  ),
+                  prefixIcon: Icon(Icons.email),
+                ),
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                controller: password,
+                validator: (value){
+                  if(value!.length < 8){
+                    return "password less than 8 chara";
+                  }
+                  return null;
+                },
+                keyboardType: TextInputType.visiblePassword,
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: "Enter your password",
+                  labelText: "Password",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: BorderSide(width: 5,
+                        color: Colors.green),
+                  ),
+                  prefixIcon: Icon(Icons.password),
+                  suffixIcon: Icon(Icons.remove_red_eye),
+                ),
+              ),
+            ),
+
+            /// TextField
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: message,
+                keyboardType: TextInputType.visiblePassword,
+                maxLines: 5,
+                decoration: InputDecoration(
+                  hintText: "send your message (option)",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: BorderSide(width: 5,
+                        color: Colors.green),
+                  ),
+                ),
+              ),
+            ),
+
+
+            SizedBox(height: 20),
+
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: InkWell(
+                onTap: (){
+                  print("email ==== ${email.text.toString()}");
+                  print("password ==== ${password.text.toString()}");
+                  print("message ==== ${message.text.toString()}");
+
+                  _keyForm.currentState!.validate();
+                },
+                child: Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.teal,
+                    borderRadius: BorderRadius.circular(20),
+
+                  ),
+                  height: 50,
+                  child: Center(
+                    child: Text("Login",
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontFamily: "Cairo",
+                          color: Colors.white
+                      ),),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
 
       ///TextFormField && form with column & padding & InkWell && Container (design 7)
       // Form(
@@ -359,8 +527,6 @@ class HomeScreen extends StatelessWidget {
       //     ],
       //   ),
       // ),
-
-
       ///TextField with && column & padding & InkWell && Container (design 6)
       // Column(
       //   crossAxisAlignment: CrossAxisAlignment.center,
@@ -436,8 +602,6 @@ class HomeScreen extends StatelessWidget {
       //     ),
       //   ],
       // ),
-
-
       /// ListView.builder with ListTile & image & CircleAvatar & Text (design 5)
       // ListView.builder(
       //   itemCount: names.length,
@@ -480,7 +644,6 @@ class HomeScreen extends StatelessWidget {
       //     );
       //  },
       // ),
-
       /// ListTile with text & image (design 4)
       //   ListTile(
       //     title: Text("Nader Mohamed",style: TextStyle(
@@ -510,7 +673,6 @@ class HomeScreen extends StatelessWidget {
       //       print("Nader mohamed");
       //     },
       //   )
-
       /// GridView.extent with Card & image & container (design 3.4)
       // GridView.extent(
       //   scrollDirection: Axis.vertical,
@@ -646,7 +808,6 @@ class HomeScreen extends StatelessWidget {
       //     ),
       //   ],
       // ),
-
       /// GridView.custom with Card & image & container (design 3.3)
       // GridView.custom(
       //     scrollDirection: Axis.vertical,
@@ -673,7 +834,6 @@ class HomeScreen extends StatelessWidget {
       //       childCount: 11,
       //     ),
       // ),
-
       /// GridView.count with Card & image & container (design 3.2)
       // GridView.count(
       //     scrollDirection: Axis.vertical,
@@ -761,7 +921,6 @@ class HomeScreen extends StatelessWidget {
       //       ),
       //   ],
       // ),
-
       /// GridView.builder with Card & image & container (design 3.1)
       // GridView.builder(
       //   itemCount: 7,
@@ -787,7 +946,6 @@ class HomeScreen extends StatelessWidget {
       //     );
       //   },
       // ),
-
       /// GridView with Card & image & container (design 3)
       // GridView(
       //   scrollDirection: Axis.vertical,
@@ -897,7 +1055,6 @@ class HomeScreen extends StatelessWidget {
       //     ),
       //   ],
       // ),
-
       /// ListView.custom with Card & image & container (design 2.3)
       // ListView.custom(
       //   scrollDirection: Axis.vertical,
@@ -925,7 +1082,6 @@ class HomeScreen extends StatelessWidget {
       //   ),
       //
       // ),
-
       /// ListView.separated with Card & image & container (design 2.2)
       // ListView.separated(
       //   itemCount: 10,
@@ -956,7 +1112,6 @@ class HomeScreen extends StatelessWidget {
       //     );
       //   },
       // ),
-
       /// ListView.builder with Card & image (design 2.1)
       // ListView.builder(
       //   itemCount: 10,
@@ -981,7 +1136,6 @@ class HomeScreen extends StatelessWidget {
       //     );
       //  },
       // ),
-
       /// ListView with InkWell &vCard & image (design 2)
       // ListView(
       //   scrollDirection: Axis.vertical,
@@ -1441,36 +1595,35 @@ class HomeScreen extends StatelessWidget {
       //     ),
       //   ],
       // ),
-
       /// InkWell with Card and image (design 1)
-      InkWell(
-        onTap: (){
-          print("Nader Mohamed");
-        },
-        onLongPress: (){
-          print("Ahmed Mohamed");
-        },
-        onDoubleTap: (){
-          print("Ramadan Mohamed");
-        },
-        child: Card(
-          color: Colors.yellow,
-          elevation: 5,
-          shadowColor: Colors.yellow,
-          clipBehavior: Clip.hardEdge,
-          shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20),
-              side: BorderSide(width: 5,color: Colors.green)
-          ),
-          child: Container(
-            width: double.infinity,
-            height: 100,
-            child: Image.asset("assets/images/food.jpg",
-              fit: BoxFit.fill,),
-
-          ),
-        ),
-      ),
+      // InkWell(
+      //   onTap: (){
+      //     print("Nader Mohamed");
+      //   },
+      //   onLongPress: (){
+      //     print("Ahmed Mohamed");
+      //   },
+      //   onDoubleTap: (){
+      //     print("Ramadan Mohamed");
+      //   },
+      //   child: Card(
+      //     color: Colors.yellow,
+      //     elevation: 5,
+      //     shadowColor: Colors.yellow,
+      //     clipBehavior: Clip.hardEdge,
+      //     shape: RoundedRectangleBorder(
+      //         borderRadius: BorderRadius.circular(20),
+      //         side: BorderSide(width: 5,color: Colors.green)
+      //     ),
+      //     child: Container(
+      //       width: double.infinity,
+      //       height: 100,
+      //       child: Image.asset("assets/images/food.jpg",
+      //         fit: BoxFit.fill,),
+      //
+      //     ),
+      //   ),
+      // ),
     );
   }
 }
